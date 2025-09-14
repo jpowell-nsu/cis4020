94ddb7a45140d3ca399252d7bad6e274293490c0
jpowell-nsu <powellja@nsula.edu>
2025-09-14 08:50:34 -0500

    Lab W05 starter code

---
+++ lastfirstLabW05/src/stack/Node.java
@@ -0,0 +1,18 @@
+package stack;
+
+public class Node {
+	
+	protected Object item;
+	protected Node next;
+
+	Node(Object newItem) {
+		item = newItem;
+	    next = null;
+	}
+
+	Node(Object newItem, Node nextNode) {
+		item = newItem;
+		next = nextNode;
+	}
+
+}
---
+++ lastfirstLabW05/src/stack/Stack.java
@@ -0,0 +1,55 @@
+package stack;
+
+public class Stack implements StackInterface {
+
+	private Node top;
+
+	public Stack() {
+		top = null;
+	}
+
+	public boolean isEmpty() {
+		return top == null;
+	}
+
+	public void push(Object newItem) {
+		top = new Node(newItem, top);
+	}
+
+	public Object pop() throws StackException {
+		if (!isEmpty()) {
+			Node temp = top;
+			top = top.next;
+			return temp.item;
+		} else {
+			throw new StackException("StackException on pop: stack empty");
+		}
+	}
+
+	public void popAll() {
+		top = null;
+	}
+
+	public Object peek() throws StackException {
+		if (!isEmpty()) {
+			return top.item;
+		} else {
+			throw new StackException("StackException on peek: stack empty");
+		}
+	}
+	
+	public void display() {
+		display(top);
+		System.out.println();
+	}  
+	
+	private void display(Node n) {
+		if (n == null) {
+			System.out.print("Base [");
+		} else {
+			display(n.next);
+			System.out.printf("(%s) ", n.item);
+		}
+	}
+
+}
