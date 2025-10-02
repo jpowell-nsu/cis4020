083c4dbdd283374ccc0c2436baf051187f1d7401
jpowell-nsu <powellja@nsula.edu>
2025-10-01 21:05:22 -0500

    Lab W08 starter

---
+++ lastfirstLabW08/src/sorter/Sorter.java
@@ -0,0 +1,46 @@
+package sorter;
+
+public class Sorter {
+	
+	private int[] values;
+
+	public Sorter(int[] values) {
+		this.values = values;
+	}
+	
+	public void sorting(String name, String order) {
+		if(name.equals("bubble")) {
+			
+		} else if(name.equals("selection")) {
+
+		} else if(name.equals("merge")) {
+
+		} else if(name.equals("quick")) {
+
+		} else if(name.equals("shell")) {
+
+		} 
+	}
+	
+	private void Bubblesort() {
+		
+	}
+	
+	private void Selectionsort() {
+		
+	}
+	
+	private void Mergesort() {
+		
+	}
+	
+	private void Quicksort() {
+		
+	}
+	
+	private void Shellsort() {
+		
+	}
+
+	
+}
